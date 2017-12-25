6bf610684234ff776f9372c29f4b26c25198641f
AamodPaud3l <paudelaamod@gmail.com>
2017-12-25 21:41:46 +0545

    Converting input set of words to uppercase

---
+++ input_to_ucase.py
@@ -0,0 +1,6 @@
+# Program to input sentence from user and print it in uppercase
+
+init_str = input("Enter some words: ")
+for each in init_str:
+    new_str = init_str.upper()
+print("In CAPITAL letters: ", new_str)
